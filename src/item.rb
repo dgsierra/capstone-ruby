bc41a24c7bdee47d33c1eb2a4a23c57b5be81f37
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-21 16:43:01 -0600

    Add date parse

--- src/item.rb
+++ src/item.rb
@@ -1,11 +1,14 @@
+require 'date'
+
 class Item
+  attr_accessor :publis_date
   def initialize(name, gender, author, source, label, publis_date, archived)
     @id = id
     @gender = gender
     @author = author
     @source = source
     @label = label
-    @publis_date = publis_date
+    @publis_date = Date.parse(publis_date)
     @archived = archived
   end
   def id()
@@ -13,6 +16,6 @@ class Item
   end
 end
 
-test1 = Item.new("test",'gender','author','source','label','publis_date','archived?')
+test1 = Item.new("test",'gender','author','source','label','2020-1-1','archived?')
 
-p test1.id
+p test1.publis_date.year
