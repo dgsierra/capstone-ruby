d0547f405bb8922760e84fe1a5c8108445441f02
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-21 15:48:40 -0500

    Add 'items' table with foreign keys to other tables

--- schema.sql
+++ schema.sql
@@ -15,3 +15,31 @@ CREATE TABLE label (
   PRIMARY KEY (id)
 )
 
+CREATE TABLE genre (
+  id INTEGER NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  items ARRAY(VARCHAR(100)) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE author (
+  id INTEGER NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  items ARRAY(VARCHAR(100)) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE items (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  genre_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  source_id INTEGER NOT NULL,
+  label_id INTEGER NOT NULL,
+  published_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  FOREIGN KEY (genre_id) REFERENCES genre(id),
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (source_id) REFERENCES source(id),
+  FOREIGN KEY (label_id) REFERENCES label(id)
+)
