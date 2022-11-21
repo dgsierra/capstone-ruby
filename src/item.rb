742164164fa4baab5481302416e694c8392baa55
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-21 16:45:12 -0600

    Add method can_be_archived?

--- src/item.rb
+++ src/item.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Item
-  attr_accessor :publis_date
+  attr_reader :publis_date
   def initialize(name, gender, author, source, label, publis_date, archived)
     @id = id
     @gender = gender
@@ -14,8 +14,13 @@ class Item
   def id()
     @id = rand(1..100000)
   end
+
+  def can_be_archived?()
+    @publis_date.year < Date.today.year - 10
+  end
 end
 
 test1 = Item.new("test",'gender','author','source','label','2020-1-1','archived?')
 
 p test1.publis_date.year
+p test1.can_be_archived?
