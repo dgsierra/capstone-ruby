3f35b55767b1bcd09b6aee7cad40ae3e41edbae9
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-21 20:12:24 -0600

    Add method move_to_archive

--- src/item.rb
+++ src/item.rb
@@ -1,26 +1,33 @@
 require 'date'
 
 class Item
-  attr_reader :publis_date
-  def initialize(name, gender, author, source, label, publis_date, archived)
-    @id = id
+  attr_reader :publis_date, :id
+  def initialize(name, gender, author, source, label, publis_date)
+    @id = rand(1..100000)
     @gender = gender
     @author = author
     @source = source
     @label = label
-    @publis_date = Date.parse(publis_date)
-    @archived = archived
-  end
-  def id()
-    @id = rand(1..100000)
+    @publis_date = Date.parse(publis_date) # format for Dates: ('2017-01-01') - YYYY-MM-DD - ISO 8601
+    @archived = nil
   end
 
   def can_be_archived?()
     @publis_date.year < Date.today.year - 10
   end
+
+  def move_to_archive()
+    if can_be_archived?()
+      @archived = true
+    end
+    p @archived
+  end
+
 end
 
-test1 = Item.new("test",'gender','author','source','label','2020-1-1','archived?')
+test1 = Item.new("test",'gender','author','source','label','2001-1-1')
 
+p test1.id
 p test1.publis_date.year
 p test1.can_be_archived?
+p test1.move_to_archive
