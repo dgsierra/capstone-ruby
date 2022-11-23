781f7976307461c06077fbb9c4258cd79d603a46
Oluyemi Paul <oluola96@gmail,com>
2022-11-23 12:16:35 +0100

    add game-data module

---
+++ src/DataManager/game_data.rb
@@ -0,0 +1,16 @@
+require '../../json'
+require '../game'
+
+module GamesData
+  def load_games
+    data = []
+    file = './data/games.json'
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |game|
+        new_game = Game.new(game['publish_date'], game['multiplayer'], game['last_played_at'], game['name'])
+        new_game.add_author(Author.new(game['author']))
+        data << new_game
+      end
+    end
+    data
+  end
